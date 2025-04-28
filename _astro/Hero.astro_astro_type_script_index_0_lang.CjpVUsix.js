565520df630d40d93007af5209993d420cf653d8
juangomez12 <juan.gomez@grupoilao.com>
2025-04-27 20:16:22 -0500

    Updates

---
+++ _astro/Hero.astro_astro_type_script_index_0_lang.CjpVUsix.js
@@ -0,0 +1,1 @@
+import{g as r,S as e}from"./ScrollTrigger.Vy8Lnz_5.js";r.registerPlugin(e);const a=["#hero-h1","#hero-p","#hero-a"];a.forEach(o=>{r.from(o,{opacity:0,y:40,duration:3,ease:"power4"})});
